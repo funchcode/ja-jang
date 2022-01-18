271de5428e0d33363ce94eba8bbe83f8c81cca66
funch <funchcode@gmail.com>
2022-01-18 21:39:21 +0900

    set: 기본 프로젝트 세팅

---
+++ ja-jang/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ja-jang
+//
+//  Created by funch on 2022/01/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ja-jang/ja_jangApp.swift
@@ -0,0 +1,17 @@
+//
+//  ja_jangApp.swift
+//  ja-jang
+//
+//  Created by funch on 2022/01/18.
+//
+
+import SwiftUI
+
+@main
+struct ja_jangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
