709505e32e6fad18e68b7ed28f96da2584743525
funch <funchcode@gmail.com>
2022-01-30 18:01:58 +0900

    feat: SecoundTimer 구조체 개발
    1초 주기로 interval 동작, 시작, 종료, 리셋 함수 제공

--- ja-jang/Views/RecordView.swift
+++ ja-jang/Views/RecordView.swift
@@ -40,20 +40,45 @@ struct CurrentRecord {
     }
 }
 
+struct SecondTimer {
+    
+    private var timer: Publishers.Autoconnect<Timer.TimerPublisher>?
+    
+    init() {
+        timer = Timer.publish(every: 1.0, on: .main, in: .common).autoconnect()
+    }
+    
+    var getTimer: Publishers.Autoconnect<Timer.TimerPublisher>? {
+        get {
+            return timer
+        }
+    }
+    
+    mutating func reset() {
+        stop()
+        start()
+    }
+    
+    mutating func start() {
+        self.timer = Timer.publish(every: 1.0, on: .main, in: .common).autoconnect()
+    }
+    
+    mutating func stop() {
+        self.timer!.upstream.connect().cancel()
+    }
+}
+
 struct RecordView: View {
     @State private var currentRecord: CurrentRecord = CurrentRecord(startTime: Date.now, currentStatus: .wakeOpenEyes)
     @State private var babyStatus: BabyStatus = .wakeOpenEyes
-    @State private var count: Int = 0
-    @State private var timer = Timer.publish(every: 1.0, on: .main, in: .common).autoconnect()
+    @State private var counter: Int = 0
+    @State private var secondTimer = SecondTimer.init()
     
     private var dateFormatter: DateFormatter = {
         let df = DateFormatter()
         df.dateFormat = "yyyy-MM-dd hh:mm:ss"
         return df
     }()
-    
-    func startRecord() {
-    }
     
     func record(status: String) {
         let timestamp = timestamp()
@@ -69,21 +94,29 @@ struct RecordView: View {
             print("current status : " + status)
             babyStatus = .sleep
             currentRecord = CurrentRecord(startTime: Date.now, currentStatus: .sleep)
+            secondTimer.reset()
+            counter = 0
             print("save current status : " + status)
         case "wakeOpenEyes":
             print("current status : " + status)
             babyStatus =  .wakeOpenEyes
             currentRecord = CurrentRecord(startTime: Date.now, currentStatus: .wakeOpenEyes)
+            secondTimer.reset()
+            counter = 0
             print("save current status : " + status)
         case "wakeCrying":
             print("current status : " + status)
             babyStatus = .wakeCrying
             currentRecord = CurrentRecord(startTime: Date.now, currentStatus: .wakeCrying)
+            secondTimer.reset()
+            counter = 0
             print("save current status : " + status)
         case "wakeWhine":
             print("current status : " + status)
             babyStatus = .wakeWhine
             currentRecord = CurrentRecord(startTime: Date.now, currentStatus: .wakeWhine)
+            secondTimer.reset()
+            counter = 0
             print("save current status : " + status)
         default:
             print("current status : " + status)
@@ -98,85 +131,72 @@ struct RecordView: View {
         return now
     }
     
-    func start() {
-        timer = Timer.publish(every: 1.0, on: .main, in: .common).autoconnect()
-    }
-    
-    func stop() {
-        timer.upstream.connect().cancel()
-    }
-    
     var body: some View {
-        VStack(alignment: .leading) {
-            
-            Text(dateFormatter.string(from: currentRecord.startTime))
-            Text(currentRecord.currentStatus.rawValue)
-            
-            Text(String(count)).onReceive(timer) { _ in
-                count += 1
-            }
-            
-            Button(action: {
-                print(Calendar.current)
-                count = 0
-                // sleep 모드
-                    // Click Event
-                    // 1. 현재 Sleep 모드인지 확인
-                    // 2. [현재 Sleep] 동작 없음.
-                    // 3. Timer 종료 이벤트 요청
-                    // 4. Timer는 이전 실행 중이던 모드 정보와 카운트 정보, 종료 날짜/시간 정보를 저장한 후 리셋
-                    // 5. Timer에 Sleep 모드로 실행.
-                    // 6. Timer는 클릭한 날짜/시간 정보와 Sleep 모드를 저장.
-                record(status: "sleep")
-            }) {
-                Text("잠")
-            }
-            
-            Button(action: {
-                print("클릭")
-                // wake 모드의 open eyes
-                    // Click Event
-                    // 1. 현재 Wake.OpenEyes 모드인지 확인.
-                    // 2. [--] 동작 없음.
-                    // 3. Timer 종료 이벤트 요청
-                    // 4. Timer는 이전 실행 중이던 정보와 카운트 정보, 종료 날짜/시간 정보를 저장한 후 리셋
-                    // 5. Timer에 Wake.OpenEyes 모드로 실행.
-                    // 6. Timer는 클릭한 날짜/시간 정보와 Wake.OpenEyes 모드를 저장
-                record(status: "wakeOpenEyes")
-            }) {
-                Text("눈뜸")
-            }
-            
-            Button(action: {
-                print("클릭")
-                // wake 모드의 crying
-                    // Click Event Wake.OpenEyes와 동일
-                record(status: "wakeCrying")
-            }) {
-                Text("움")
-            }
-            
-            Button(action: {
-                print("클릭")
-                // wake 모드의 whine
-                    // Click Event Wake.OpenEyes와 동일
-                record(status: "wakeWhine")
-            }) {
-                Text("칭얼")
-            }
-            
-            Button(action: {
-                print("클릭")
-                // skill 이벤트의 encourage
-                    // Click Event
-                    // 1. 현재 날짜/시간 정보가 입력된 모달 창 Open
+            VStack(alignment: .center) {
+                Text(dateFormatter.string(from: currentRecord.startTime))
+                HStack(alignment: .top) {
+                    Text(currentRecord.currentStatus.rawValue)
+                    Text(String(counter)).onReceive(secondTimer.getTimer!) { _ in
+                        counter = Calendar.current.dateComponents([.second], from: currentRecord.startTime, to: Date.now).second!
+                    }
+                }
+
+                Button(action: {
+                    print(Calendar.current)
+                    counter = 0
+                        // Timer는 이전 실행 중이던 모드 정보와 카운트 정보, 종료 날짜/시간 정보를 저장한 후 리셋
+                        // 6. Timer는 클릭한 날짜/시간 정보와 Sleep 모드를 저장.
+                    record(status: "sleep")
+                }) {
+                    Text("잠")
+                }
+
+                Button(action: {
+                    print("클릭")
+                        // Timer는 이전 실행 중이던 정보와 카운트 정보, 종료 날짜/시간 정보를 저장한 후 리셋
+                        // 6. Timer는 클릭한 날짜/시간 정보와 Wake.OpenEyes 모드를 저장
+                    record(status: "wakeOpenEyes")
+                }) {
+                    Text("눈뜸")
+                }
+
+                Button(action: {
+                    print("클릭")
+                    // wake 모드의 crying
+                        // Click Event Wake.OpenEyes와 동일
+                    record(status: "wakeCrying")
+                }) {
+                    Text("움")
+                }
+
+                Button(action: {
+                    print("클릭")
+                    // wake 모드의 whine
+                        // Click Event Wake.OpenEyes와 동일
+                    record(status: "wakeWhine")
+                }) {
+                    Text("칭얼")
+                }
+
+                Button(action: {
+                    print("클릭")
+                        // 현재 날짜/시간 정보가 입력된 모달 창 Open
+                        // 2. 시간 수정이 필요한 경우 수정
+                        // 3. 모달 창의 '등록' 버튼 누를 시 이벤트 발생
+                        // 4. 등록 이벤트 시 입력한 날짜/시간 정보와 Skill.Encourage 등록
+                }) {
+                    Text("격려")
+                }
+
+                Button(action: {
+                    print("클릭")
+                    // 현재 날짜/시간 정보가 입력된 모달 창 Open
                     // 2. 시간 수정이 필요한 경우 수정
                     // 3. 모달 창의 '등록' 버튼 누를 시 이벤트 발생
                     // 4. 등록 이벤트 시 입력한 날짜/시간 정보와 Skill.Encourage 등록
-                
-            }) {
-                Text("격려")
-            }
+                }) {
+                    Text("쪽쪽이")
+                }
         }
     }
 }
