115bd12c2407b6baf320b147dc0fd2f46a4bb580
funch <funchcode@gmail.com>
2022-02-25 23:30:37 +0900

    style: RecordList Section-Text 구성, Background 색상 지정
    현재 가데이터로 RecordList 목록 표기 중, FireStore 데이터 조회 코드로 수정해야 함

---
+++ ja-jang/Views/RecordViews.swift
@@ -0,0 +1,20 @@
+//
+//  RecordViews.swift
+//  ja-jang
+//
+//  Created by funch on 2022/02/24.
+//
+
+import SwiftUI
+
+struct RecordViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecordViews_Previews: PreviewProvider {
+    static var previews: some View {
+        RecordViews()
+    }
+}
---
+++ ja-jang/Views/Share/RecordList.swift
@@ -0,0 +1,20 @@
+//
+//  RecordList.swift
+//  ja-jang
+//
+//  Created by funch on 2022/02/24.
+//
+
+import SwiftUI
+
+struct RecordList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecordList_Previews: PreviewProvider {
+    static var previews: some View {
+        RecordList()
+    }
+}
